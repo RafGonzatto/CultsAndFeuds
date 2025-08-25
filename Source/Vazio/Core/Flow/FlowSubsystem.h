6f5e9abb1b76bcbc5dff962b1d066e97d57d36dc
RafGonzatto <rafaelgonzatto181@gmail.com>
2025-08-25 08:44:35 -0300

    Fix: Add Config=Game to FlowSubsystem UCLASS to resolve build error

---
+++ Source/Vazio/Core/Flow/FlowSubsystem.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "FlowSubsystem.generated.h"
+
+UENUM(BlueprintType)
+enum class EVazioMode : uint8
+{
+	MainMenu,
+	City,
+	Battle
+};
+
+UCLASS()
+class VAZIO_API UFlowSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	// Ajuste os nomes dos mapas conforme você criar no Content
+	UPROPERTY(Config)
+	FName MainMenuMap = TEXT("MainMenu");
+
+	UPROPERTY(Config)
+	FName CityMap = TEXT("City_P");
+
+	// Carrega por nome simples (os dois mapas devem estar em /Game/Maps/.. com esses nomes)
+	UFUNCTION()
+	bool OpenLevelByName(FName MapName);
+
+	// Atalhos
+	UFUNCTION() bool OpenMainMenu();
+	UFUNCTION() bool OpenCity();
+};
